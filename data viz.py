fa4384e7b583206efb77d4646f11a8fdc58830d7
ederelfilali <138714077+ederelfilali@users.noreply.github.com>
2023-07-16 00:06:12 +0100

    Update data viz.py

--- data viz.py
+++ data viz.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 
-# Sample data
+
 x = [10, 25, 37, 49, 75]
 y = [2, 4, 6, 8, 10]
 
-# Create the plot
+
 plt.plot(x, y)
 
-# Set labels and title
+
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Exemple')
 
-# Display the plot
+
 plt.show()
